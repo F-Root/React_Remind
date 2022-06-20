fe987bd4ed27acecb9423982f2de2dd7c7be835b
RooT_Kim <bdd3452@gmail.com>
2022-06-20 11:19:12 +0900

    React Remind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
+import { useState, useEffect } from 'react';
+import { Link, Routes, Route, useParams } from 'react-router-dom';
+
 function Header() {
   return (
     <header>
       <h1>
-        <a href='/'>WEB</a>
+        <Link to='/'>WEB</Link>
       </h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props) {
   return (
     <nav>
-      <ol></ol>
+      <ol>
+        {props.data.map((e) => {
+          return (
+            <li key={e.id}>
+              <Link to={`/read/${e.id}`}>{e.title}</Link>
+            </li>
+          );
+        })}
+      </ol>
     </nav>
   );
 }
 
+function Read(props) {
+  const param = useParams();
+  const id = Number(param.id);
+  const [topic, setTopic] = useState({ title: null, body: null });
+
+  const refreshTopic = async () => {
+    const response = await fetch('http://localhost:3333/topics/' + id);
+    const result = await response.json();
+    setTopic(result);
+  };
+
+  useEffect(() => {
+    refreshTopic();
+  }, []);
+
+  return (
+    <article>
+      <h2>{topic.title}</h2>
+      <p>{topic.body}</p>
+    </article>
+  );
+}
+
 function App() {
+  const [topics, setTopics] = useState([]);
+  const refreshTopics = async () => {
+    const response = await fetch('http://localhost:3333/topics');
+    const result = await response.json();
+    setTopics(result);
+  };
+
+  useEffect(() => {
+    refreshTopics();
+  }, []);
+
+  console.log(topics);
+
   return (
     <div>
       <Header></Header>
-      <Nav></Nav>
+      <Nav data={topics}></Nav>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <h2>Welcome</h2>
+              <p>Hello, React!</p>
+            </>
+          }
+        ></Route>
+        <Route path='/read/:id' element={<Read></Read>}></Route>
+      </Routes>
     </div>
   );
 }
